d8a0592fd41f9472400d8e756fbc3d3390b7b587
Ewerson.Brandina <ewerson.brandina@hotmail.com.br>
2022-06-03 09:05:58 -0300

    Alteração 03-06-2022

--- Calculadora 01-06-2022.py
+++ Calculadora 01-06-2022.py
@@ -1,36 +1,17 @@
 from tkinter import * 
-#back-end
-#inserir os valores a serem calculados
-def zero():
-    in0['text'] = in0['text']+'0'
-def um():
-    in0['text'] = in0['text']+'1'
-def dois():
-    in0['text'] = in0['text']+'2'
-def tres():
-    in0['text'] = in0['text']+'3'
-def quatro():
-    in0['text'] = in0['text']+'4'
-def cinco():
-    in0['text'] = in0['text']+'5'
-def seis():
-    in0['text'] = in0['text']+'6'
-def sete():
-    in0['text'] = in0['text']+'7'
-def oito():
-    in0['text'] = in0['text']+'8'
-def nove():
-    in0['text'] = in0['text']+'9'
 #salvar os valores inseridos para calculalos
-def mais():
-    global a
-    a=in0['text']
-    in0['text']=''
-def igual():
-    global a
-    b=in0['text']
-    x= a + b
+def entrada(valor):
+    in0['text'] += valor   
+       
+
+def resultado():
+    x=eval(in0['text'])
     in0['text']=x
+
+def limpar():
+    in0['text'] = ''
+
+
     
 #front-end
 root = Tk()
@@ -46,47 +27,48 @@ root.grid_columnconfigure(0, weight=1)
 root.grid_columnconfigure(1, weight=1)
 root.grid_columnconfigure(2, weight=1)
 root.grid_columnconfigure(3, weight=1)
+root.geometry('320x500')
 #Label and Name
-in0 = Label (root, text='0', font='Arial 25')
+in0 = Label (root, text='', font='Arial 25')
 in0.grid(row=1, column=0, columnspan=4, sticky=NSEW)
 lb0 = Label (root, text='CALCULADORA', font='Arial 25', fg='green')
 lb0.grid(row=0, column=0, columnspan=4, sticky=NSEW)
 lb0.config(bg='black')
 #widgets numéricos
-bt0 = Button (root, text='0', font= 'Arial 25')
-bt1 = Button (root, text='1', font= 'Arial 25')
-bt2 = Button (root, text='2', font= 'Arial 25')
-bt3 = Button (root, text='3', font= 'Arial 25')
-bt4 = Button (root, text='4', font= 'Arial 25')
-bt5 = Button (root, text='5', font= 'Arial 25')
-bt6 = Button (root, text='6', font= 'Arial 25')
-bt7 = Button (root, text='7', font= 'Arial 25')
-bt8 = Button (root, text='8', font= 'Arial 25')
-bt9 = Button (root, text='9', font= 'Arial 25')
+bt0 = Button (root, text='0', font= 'Arial 25', command=lambda: entrada('0'))
+bt1 = Button (root, text='1', font= 'Arial 25', command=lambda: entrada('1'))
+bt2 = Button (root, text='2', font= 'Arial 25', command=lambda: entrada('2'))
+bt3 = Button (root, text='3', font= 'Arial 25', command=lambda: entrada('3'))
+bt4 = Button (root, text='4', font= 'Arial 25', command=lambda: entrada('4'))
+bt5 = Button (root, text='5', font= 'Arial 25', command=lambda: entrada('5'))
+bt6 = Button (root, text='6', font= 'Arial 25', command=lambda: entrada('6'))
+bt7 = Button (root, text='7', font= 'Arial 25', command=lambda: entrada('7'))
+bt8 = Button (root, text='8', font= 'Arial 25', command=lambda: entrada('8'))
+bt9 = Button (root, text='9', font= 'Arial 25', command=lambda: entrada('9'))
 #widgets simbolos
-bt10 = Button (root, text='.', font= 'Arial 25')
-bt11 = Button (root, text='=', font= 'Arial 25')
-bt12 = Button (root, text='+', font= 'Arial 25')
-bt13 = Button (root, text='-', font= 'Arial 25')
-bt14 = Button (root, text='x', font= 'Arial 25')
-bt15 = Button (root, text='/', font= 'Arial 25')
-bt16 = Button (root, text='√', font= 'Arial 25')
-bt17 = Button (root, text='x²', font= 'Arial 25')
-bt18 = Button (root, text='%', font= 'Arial 25')
-bt19 = Button (root, text='⌫', font= 'Arial 25')
-bt20 = Button (root, text='C', font= 'Arial 25')
+bt10 = Button (root, text='.', font= 'Arial 25', command=lambda: entrada('.'))
+bt11 = Button (root, text='=', font= 'Arial 25', command=lambda: resultado())
+bt12 = Button (root, text='+', font= 'Arial 25', command=lambda: entrada('+'))
+bt13 = Button (root, text='-', font= 'Arial 25', command=lambda: entrada('-'))
+bt14 = Button (root, text='x', font= 'Arial 25', command=lambda: entrada('*'))
+bt15 = Button (root, text='/', font= 'Arial 25', command=lambda: entrada('/'))
+bt16 = Button (root, text='√', font= 'Arial 25', command=lambda: entrada('√'))
+bt17 = Button (root, text='x²', font= 'Arial 25', command=lambda: entrada('x²'))
+bt18 = Button (root, text='%', font= 'Arial 25', command=lambda: entrada('%'))
+bt19 = Button (root, text='⌫', font= 'Arial 25', command=lambda: limpar())
+bt20 = Button (root, text='C', font= 'Arial 25', command=lambda: limpar())
 
-#root.bind('10', lambda event: ponto())
-root.bind('=', lambda event: igual())
-root.bind('+', lambda event: mais())
-#root.bind('13', lambda event: menos())
-#root.bind('14', lambda event: vezes())
-#root.bind('15', lambda event: dividir())
-#root.bind('16', lambda event: raiz())
-#root.bind('17', lambda event: elevar())
-#root.bind('18', lambda event: porcentagem())
-#root.bind('19', lambda event: apagarTudo())
-#root.bind('19', lambda event: apagar())
+#root.bind('10', lambda event: entrada())
+#root.bind('=', lambda event: entrada())
+#root.bind('+', lambda event: entrada())
+#root.bind('13', lambda event: entrada())
+#root.bind('14', lambda event: entrada())
+#root.bind('15', lambda event: entrada())
+#root.bind('16', lambda event: entrada())
+#root.bind('17', lambda event: entrada())
+#root.bind('18', lambda event: entrada())
+#root.bind('19', lambda event: entrada())
+#root.bind('19', lambda event: entrada())
 
 #Números
 bt0.grid(row=7, column=0, columnspan=2, sticky=NSEW)
@@ -112,15 +94,15 @@ bt18.grid(row=3, column=0, sticky=NSEW)
 bt19.grid(row=2, column=2, columnspan=2, sticky=NSEW)
 bt20.grid(row=2, column=0, columnspan=2, sticky=NSEW)
 #vincular as teclas do teclado
-root.bind('0', lambda event: zero())
-root.bind('1', lambda event: um())
-root.bind('2', lambda event: dois())
-root.bind('3', lambda event: tres())
-root.bind('4', lambda event: quatro())
-root.bind('5', lambda event: cinco())
-root.bind('6', lambda event: seis())
-root.bind('7', lambda event: sete())
-root.bind('8', lambda event: oito())
-root.bind('9', lambda event: nove())
+root.bind('0', lambda event: entrada('0'))
+root.bind('1', lambda event: entrada('1'))
+root.bind('2', lambda event: entrada('2'))
+root.bind('3', lambda event: entrada('3'))
+root.bind('4', lambda event: entrada('4'))
+root.bind('5', lambda event: entrada('5'))
+root.bind('6', lambda event: entrada('6'))
+root.bind('7', lambda event: entrada('7'))
+root.bind('8', lambda event: entrada('8'))
+root.bind('9', lambda event: entrada('9'))
 #run #Função expecífica para manter a janela aparecendo.
 root.mainloop()
